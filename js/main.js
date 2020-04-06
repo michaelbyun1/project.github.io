dbceb3c177275babd75746f3084eb9bbd388e4a8
michaelbyun1 <52934694+michaelbyun1@users.noreply.github.com>
2020-04-05 23:57:40 -0400

    Included code

--- js/main.js
+++ js/main.js
@@ -39,3 +39,42 @@ carouselSlide.addEventListener('transitionend', () => {
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
   }
 });
+
+
+//Infinite content
+let haveHitBottom = false
+
+let theStateOfTheInterface = (event) => { 
+  
+  let winH = document.documentElement.clientHeight
+  let winW = document.documentElement.clientWidth
+  let docH = document.documentElement.scrollHeight
+  let docW = document.documentElement.scrollWidth
+  let winY = window.scrollY
+  let winX = window.scrollX
+  let maxY = docH - winH
+  let maxX = docW - winW
+  let pctY = Math.round(winY / Math.max(maxY, 1) * 100)
+  let pctX = Math.round(winX / Math.max(maxX, 1) * 100)
+
+  if (winY >= maxY) {
+    document.querySelector('#content').innerHTML += `
+      <div class="new-content">
+        <h2>Thank you.</h2>
+      </div>
+      `
+  }
+
+  document.querySelector('.panel').innerHTML = `
+    <li>The window is <strong>${winH}</strong> px tall, <strong>${winW}</strong> px wide</li>
+    <li>The document is <strong>${docH}</strong> px tall, <strong>${docW}</strong> px wide</li>
+    <li>The window has been scrolled vertically <strong>${winY}</strong> px of <strong>${maxY}</strong> px (<strong>${pctY}</strong> %)</li>
+    <li>The window has been scrolled horizontally <strong>${winX}</strong> px of <strong>${maxX}</strong> px (<strong>${pctX}</strong> %)</li>
+  `
+
+}
+
+
+window.addEventListener('load', theStateOfTheInterface)
+window.addEventListener('scroll', theStateOfTheInterface)
+window.addEventListener('resize', theStateOfTheInterface)
